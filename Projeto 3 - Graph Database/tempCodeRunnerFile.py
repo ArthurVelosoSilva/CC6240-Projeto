aaf9489392d228d05e1748ebfda36ecdeec4530d
ArthurVelosoSilva <105575026+ArthurVelosoSilva@users.noreply.github.com>
2025-05-24 05:23:50 -0300

    Delete Projeto 3 - Graph Database/tempCodeRunnerFile.py

--- Projeto 3 - Graph Database/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
-with driver.session() as session:
-    print("Criando nós...")
-    executar_arquivo_cypher(session, "criar_nos.cypher")
-    print("Criando relacionamentos...")
-    executar_arquivo_cypher(session, "criar_relacionamentos.cypher")
-    print("Executando consultas...")
-    executar_consultas(session, "consultas_grafo.cypher")
